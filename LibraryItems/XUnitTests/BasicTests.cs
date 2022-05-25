679bc83fef4e7f365edc27c61ee60e07c6266785
Zakiyah Yaqoob <zakiyah.yaqoob@avanade.com>
2022-05-25 17:05:18 +0100

    get one basic unit test up

--- LibraryItems/XUnitTests/BasicTests.cs
+++ LibraryItems/XUnitTests/BasicTests.cs
@@ -1,19 +1,26 @@
+using LibraryItems.Controllers;
 using LibraryItems.Models;
+using Moq;
 using Xunit;
 
 namespace XUnitTests
 {
     public class BasicTests
     {
+        private LibraryItemController controller;
         [Fact]
-        public void Add_1_Item_Ok()
-        {
-            LibraryItem libraryItem = new LibraryItem();
-            libraryItem.Name = "The Hunger Games: Mockingjay";
-        }
-
         public void Delete_1_Item_Ok()
         {
+            //arrange
+            var libraryMock = new Mock<LibraryItemController>(); //create a mock
+            //act
+           // libraryMock.Setup(s => s.Delete(It.IsAny<int>()));
+            var result = controller.Delete(It.IsAny<int>());
+            //assert
+            Assert.NotNull(result);
+
         }
+
+        
     }
 }
